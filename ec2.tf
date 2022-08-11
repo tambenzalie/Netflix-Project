944cf5f4f2303601a95e1d7f8e6a034ad2aefb02
Howard <howie8183@hotmail.com>
2022-08-11 03:21:36 +0100

    Commit message modified as per jira task

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,6 @@
 #Code to create ec2 instance 
 #this is new code
+#Some changes made to code
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
--- s3.tf
+++ s3.tf
@@ -1,3 +1,4 @@
+#bucket is supposed to be private
 resource "aws_s3_bucket" "b" {
   bucket = "my-tf-test-bucket"
 
