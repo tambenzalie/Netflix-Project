6e34f890447473337f99c7706e66d42fb8784a6d
Howard <howie8183@hotmail.com>
2022-08-11 01:32:13 +0100

    Adding Code to Create EC2 Instance

---
+++ ec2.tf
@@ -0,0 +1,10 @@
+#Code to create ec2 instance 
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
