98290c2d987343eb5afcbbe648317758bcba119d
Howard <howie8183@hotmail.com>
2022-08-11 02:21:56 +0100

    Added new Infra

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 #Code to create ec2 instance 
-
+#this is new code
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
---
+++ role.tf
@@ -0,0 +1,2 @@
+#Code to create ec2 instance 
+#this is new code
